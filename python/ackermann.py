35eb857d4ab7f86833ffa533b89f02056546bbb5
Paul Curry <pcurry@gmail.com>
2011-12-25 04:27:53 -0500

    Well, working now, and looking at shortcuts

--- python/ackermann.py
+++ python/ackermann.py
@@ -11,12 +11,12 @@ def ackermann_peter(m, n):
     if m < 0 or n < 0:
         raise InvalidInputException("m and n both need to be greater than 0")
     elif m == 0:
-        return n
+        return n + 1
     elif n == 0:
         return ackermann_peter(m - 1, 1)
     else:
         return ackermann_peter(m - 1, ackermann_peter(m, n - 1))
-    
+
 
 def ackermann_peter_faster(m, n):
     """
@@ -25,14 +25,17 @@ def ackermann_peter_faster(m, n):
     if m < 0 or n < 0:
         raise InvalidInputException("m and n both need to be greater than 0")
     elif m == 0:
-        return n
+        return n + 1
+    elif m == 1:
+        return n + 2
+    elif m == 2:
+        return 2 * n + 3
+    elif m == 3:
+        return (2 ** (n + 3)) - 3
     elif n == 0:
-        if m <= 2:
-            return 1
-        else:
-            return ackermann_peter(m - 2, ackermann_peter(m - 1, 0))
+        return ackermann_peter_faster(m - 1, 1)
     else:
-        return ackermann_peter(m - 1, ackermann_peter(m, n - 1))
+        return ackermann_peter_faster(m - 1, ackermann_peter_faster(m, n - 1))
 
 
 
@@ -41,20 +44,23 @@ def ackermann_peter_faster_memo(m, n, memo={(1, 1): 1, (2, 0): 1}):
 
     """
     if m < 0 or n < 0:
-        raise InvalidInputException("m and n both need to be greater than 0")
+        raise InvalidInputException("m and n both need to be greater than or equal to 0")
     elif m == 0:
-        return n
+        return n + 1
+    elif m == 1:
+        return n + 2
+    elif m == 2:
+        return 2 * n + 3
     elif (m, n) in memo:
         return memo[(m, n)]
+    elif m == 3:
+        result = (2 ** (n + 3)) - 3
+        memo[(m, n)] = result
+        return result
     elif n == 0:
-        if m <= 2:
-            memo[(m, n)] = 1
-            return 1
-        else:
-            new_n = ackermann_peter_faster_memo(m - 1, 0)
-            result = ackermann_peter_faster_memo(m - 2, new_n)
-            memo[(m, n)] = result
-            return result
+        result = ackermann_peter_faster_memo(m - 1, 1)
+        memo[(m, n)] = result
+        return result
     else:
         new_n = ackermann_peter_faster_memo(m, n - 1)
         result = ackermann_peter_faster_memo(m - 1, new_n)
@@ -62,17 +68,17 @@ def ackermann_peter_faster_memo(m, n, memo={(1, 1): 1, (2, 0): 1}):
         return result
 
 
-def print_ackermann_peter_calls(m, n):
-    format_string = "ackermann_peter(%d, %s)"
-    n_string = str(n)
-    ms = [m]
-    ns = [n]
-    keep_printing = True
-    while keep_printing:
-        m2 = ms.pop()
-        m1 = m2 - 1
-        n1 = ns.pop()
-        if m2 == 0 and len(ms) == 0 and len(ns) == 0:
-            print n1
-            keep_printing = False
-        elif m2 == 0 
+# def print_ackermann_peter_calls(m, n):
+#     format_string = "ackermann_peter(%d, %s)"
+#     n_string = str(n)
+#     ms = [m]
+#     ns = [n]
+#     keep_printing = True
+#     while keep_printing:
+#         m2 = ms.pop()
+#         m1 = m2 - 1
+#         n1 = ns.pop()
+#         if m2 == 0 and len(ms) == 0 and len(ns) == 0:
+#             print n1
+#             keep_printing = False
+#         elif m2 == 0
--- python/test_ackermann.py
+++ python/test_ackermann.py
@@ -8,16 +8,20 @@ class TestAckermannPeter(unittest.TestCase):
     def setUp(self):
         self.ap = ackermann.ackermann_peter
         self.test_name = self.ap.__name__
-        
-        
+
+
     def test_m_zero_n_under_10(self):
         for x in xrange(10):
+            expected = x + 1
             result = self.ap(0, x)
             self.assertTrue(result)
-            self.assertEquals(x, 
+            self.assertEquals(expected,
                               result,
-                              "%s(0, %d) returned %d rather than expected %d." % (self.test_name, x, result, x))
-    
+                              "%s(0, %d) returned %d rather than expected %d." % (self.test_name,
+                                                                                  expected,
+                                                                                  result,
+                                                                                  expected))
+
 
 
 if __name__ == "__main__":
